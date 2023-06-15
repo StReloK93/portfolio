9c1dc76bfb28ea10428658cb47073b4af234ac4a
strelok93 <strelok0493@gmail.com>
2023-06-15 14:37:24 +0500

    master

---
+++ resources/js/Components/ResumeVariantOne.vue
@@ -0,0 +1,100 @@
+<template>
+    <aside class="print h-[297mm] flex flex-wrap">
+        <div class="w-1/2 pr-4">
+            <header class="flex justify-between items-start h-28">
+                <main>
+                    <h3 class="text-gray-500 leading-none">
+                        Aziz Mardonovich
+                    </h3>
+                    <p class="text-[10px] text-gray-400">
+                        Laravel & Vue - Developer
+                    </p>
+                </main>
+                <main class="flex text-xs text-gray-400 mr-16">
+                    <div>
+                        <h3>Addres</h3>
+                    </div>
+                    <div class="border-l ml-2 pl-2 ">
+                        <p class="tracking-wider">Uzbekistan</p>
+                        <p>Uchquduq</p>
+                        <p>13-19-34</p>
+                    </div>
+                </main>
+            </header>
+            <aside class="mb-5">
+                <h3 class="uppercase text-sm text-gray-600 border-b pb-1 mb-3">
+                    Personal statement
+                </h3>
+                <p class="text-xs text-gray-400 mb-1.5 pr-4">
+                    Lorem ipsum dolor sit amet consectetur adipisicing elit.
+                    In voluptatum sint exercitationem officiis eveniet vitae
+                    at beatae aliquam amet quis similique placeat,
+                </p>
+                <p class="text-xs text-gray-400  pr-4">
+                    Lorem ipsum dolor sit amet consectetur adipisicing
+                    In voluptatum sint exercitationem officiis eveniet 
+                    at beatae aliquam amet quis similique placeat
+                </p>
+            </aside>
+            <aside >
+                <h3 class="uppercase text-sm text-gray-600 border-b pb-1 mb-3">
+                    My specialities
+                </h3>
+                <div class>
+
+                </div>
+                <p class="text-xs text-gray-400  pr-4">
+                    Lorem ipsum dolor sit amet consectetur adipisicing
+                    In voluptatum sint exercitationem officiis eveniet 
+                    at beatae aliquam amet quis similique placeat
+                </p>
+            </aside>
+        </div>
+        <div class="w-1/2 pl-4 text-xs text-gray-400">
+            <header class="flex justify-between items-start h-28">
+                <main class="flex -ml-[52px]">
+                    <div>
+                        <h3>Contacts</h3>
+                    </div>
+                    <div class="border-l ml-2 pl-2">
+                        <p><i class="fa-sharp fa-solid fa-phone mr-1"></i> +99893 660 09 60</p>
+                        <p><i class="fa-solid fa-envelope mr-1"></i> Strelok0493@gmail.com</p>
+                        <p><i class="fa-brands fa-telegram mr-1"></i> @ruzzifer</p>
+                    </div>
+                </main>
+                <div>
+                    <a href="https://ruzzifer.site" class="italic">ruzzifer.site</a>
+                </div>
+            </header>
+            <aside>
+                <h3 class="uppercase text-sm text-gray-600 border-b pb-1 mb-3">
+                    Work experience
+                </h3>
+                <div class="border-l border-gray-500 py-3">
+                    <main class="text-xs text-gray-400 mb-4 px-3 relative">
+                        <div class="w-[10px] h-[10px] bg-gray-500 rounded-full border-2 border-white absolute top-1/2 -translate-y-1/2 -left-[6px]"></div>
+                        <p class="text-gray-600">From september 2019 to september 2024</p>
+                        <p>Business explorer</p>
+                        <div>Frontend developer</div>
+                    </main>
+                    <main class="text-xs text-gray-400 mb-4 px-3 relative">
+                        <div class="w-[10px] h-[10px] bg-gray-500 rounded-full border-2 border-white absolute top-1/2 -translate-y-1/2 -left-[6px]"></div>
+                        <p class="text-gray-600">From september 2019 to september 2020</p>
+                        <p>Business explorer</p>
+                        <div>Frontend developer</div>
+                    </main>
+                    <main class="text-xs text-gray-400 px-3 relative">
+                        <div class="w-[10px] h-[10px] bg-gray-500 rounded-full border-2 border-white absolute top-1/2 -translate-y-1/2 -left-[6px]"></div>
+                        <p class="text-gray-600">From september 2015 to september 2019</p>
+                        <div>Backend developer</div>
+                        at beatae aliquam amet quis similique placeat,
+                    </main>
+                </div>
+            </aside>
+        </div>
+    </aside>
+</template>
+
+<script setup lang="ts">
+
+</script>
